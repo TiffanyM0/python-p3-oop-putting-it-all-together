911ac649f87362ac2948c84d420837a4ad953bb9
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-02-02 13:45:14 +0300

    complete

--- lib/book.py
+++ lib/book.py
@@ -2,5 +2,21 @@
 
 class Book:
     pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count= page_count
     
+    def getPageCount(self):
+        return self._page_count 
+    
+    def setPageCount(self, page_count):
+        if (type(page_count) in (int, float)):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(getPageCount, setPageCount)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,22 @@
 
 class Shoe:
     pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+    
+    def getSize(self):
+        return self._size
+    
+    def setSize(self, size):
+        if (type(size) in (int, float)):
+            self._size = size
+        else:
+            print("size must be an integer")
+    size = property(getSize, setSize)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New" 
+
+    
